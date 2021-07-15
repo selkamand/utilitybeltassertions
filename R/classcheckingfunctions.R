97322364d6b110ce8898e1b03a60cdd7cc3045ab
selkamand <sam.elkamand@gmail.com>
2021-07-15 18:17:36 +1000

    added class_is function

---
+++ R/classcheckingfunctions.R
@@ -0,0 +1,14 @@
+#' class_is
+#'
+#' Check if object has a particular class
+#'
+#' @param object object whose class you want to check (object)
+#' @param tested_class class (string)
+#'
+#' @return TRUE if object class matches tested_class. FALSE if not.
+#' @export
+#'
+class_is <- function(object, tested_class){
+  assert_that(is.character(tested_class))
+  all(class(object) == tested_class)
+}
